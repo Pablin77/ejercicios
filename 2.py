6ce7fcaec1502085cc202646eb6f3864cf969221
Pablin77 <117429287+Pablin77@users.noreply.github.com>
2024-08-13 10:56:30 -0300

    Update 2.py
    Se realizan modificaciones para contemplar los casos de por ejemplo: Sci-Fi y Reality-TV.
    
    Se actualiza de tal forma que se contemple el caso de que, por pagina puedan existir distintos resultados.

--- 2.py
+++ 2.py
@@ -12,35 +12,40 @@ import json
 
 
 def bestInGenre(s):
-    """
-    Funcion que recorre las paginas y analiza los elementos de tal manera que devuelve la serie
-    de TV con mayor rating, ordenando los resultados en el caso que exista un empate.
-    """
-    genero = s.capitalize()
-    url = "https://jsonmock.hackerrank.com/api/tvseries"
-    response = requests.get(url)
-    data = response.json()
-    total_page = data['total_pages']
-    per_page = data['per_page']
-    imdb_rating = float(0)
-    for page in range(1, total_page + 1):
-        content = requests.get(url+"?page="+str(page)).json()
-        for per_p in range(per_page):
-            if genero in content['data'][per_p]['genre']:
-                rating_content = float(content['data'][per_p]['imdb_rating'])
-                if imdb_rating < rating_content:
-                    name= []
-                    name.append(content['data'][per_p]['name'])
-                    imdb_rating = rating_content
-                elif imdb_rating == rating_content:
-                    name.append(content['data'][per_p]['name'])
-    if imdb_rating == 0:
-        return('No hay series con el genero '+ genero)
-    else:
-        name = sorted(name)
-        return name
+   """
+   Funcion que recorre las paginas y analiza los elementos de tal manera que devuelve la serie
+   de TV con mayor rating, ordenando los resultados en el caso que exista un empate.
+   """
+   genero = s.capitalize()
+   url = "https://jsonmock.hackerrank.com/api/tvseries"
+   response = requests.get(url)
+   data = response.json()
+   total_page = data['total_pages']
+   imdb_rating = float(0)
+   for page in range(1, total_page + 1):
+       content = requests.get(url+"?page="+str(page)).json()
+       # Actualizo cantidad de resultados por pagina 
+       per_page = content['per_page']
+       for per_p in range(per_page):
+           # Separo los generos para tratarlos por separado
+           generos = content['data'][per_p]['genre'].split(", ")
+           # Convierto cada genero al formato capitalize para solucionar el caso de Sci-Fi y Reality-TV
+           generos = [xgeneros.capitalize() for xgeneros in generos]
+           if genero in generos:
+               rating_content = float(content['data'][per_p]['imdb_rating'])
+               if imdb_rating < rating_content:
+                   name= []
+                   name.append(content['data'][per_p]['name'])
+                   imdb_rating = rating_content
+               elif imdb_rating == rating_content:
+                   name.append(content['data'][per_p]['name'])
+   if imdb_rating == 0:
+       return('No hay series con el genero '+ s)
+   else:
+       name = sorted(name)
+       return name
 
 
 if __name__ == "__main__":
-    genero = input('Ingrese genero: ')
-    print(bestInGenre(genero))
+   genero = input('Ingrese genero: ')
+   print(bestInGenre(genero))
